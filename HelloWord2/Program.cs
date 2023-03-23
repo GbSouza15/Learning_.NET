c26717dd98709e6b6eb1d3ea3f9b47af792e3b56
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-03-23 15:26:01 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace HelloWord2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gabriel Souza";
+            string profession = "Front End";
+            int age = 19;
+            string skills = "React, Tailwind e C#";
+
+            Console.WriteLine($"Olá, meu nome é {name}, tenho {age}. Sou {profession} dev e minhas principais habilidades são: {skills}!");
+        }
+    }
+}
